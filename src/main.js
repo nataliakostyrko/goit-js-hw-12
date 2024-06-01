c05f44c58e70639fe4ba00a3f2d81529afb4385d
nataliakostyrko <nataliskrip1408@gmail.com>
2024-06-01 15:09:48 +0200

    js

--- src/main.js
+++ src/main.js
@@ -11,12 +11,17 @@ const imagesList = document.querySelector('.js-images-container');
 const loadMoreButton = document.querySelector('.load-more');
 
 let currentPage = 1;
-let query;
+let query = '';
+let totalPages = 0;
+
+
 
 loadMoreButton.addEventListener('click', handleUpdate);
 formElem.addEventListener('submit', async event => {
     event.preventDefault();
+    hideloadMoreButton();
     imagesList.innerHTML = '';
+    showLoader();
     
     const query = event.target.elements.query.value.trim();
 
@@ -25,41 +30,81 @@ formElem.addEventListener('submit', async event => {
             title: 'Error',
             message: 'Please enter a search query',
         });
+        hideLoader();
         return;
     }
-    loader.classList.remove('hidden');
-    loader.classList.add('hidden');
-    showLoader();
+    
+    
     
     try {
-        const data = await searchImages(guery, currentPage);
-   
+        currentPage = 1;
+        const data = await searchImages(query, currentPage);
         if (data.hits.length > 0) {
             addImage(data.hits);
-            loadMoreButton.style.display = 'block';
+            totalPages = Math.ceil(data.totalHits / 15);
+            checkloadMoreButton();
+        
         } else {
-            iziToast.warning({
-                title: 'Warning',
-                message: 'Sorry, there are no images matching your search query. Please try again!',
-            }, 2000);
-            hideLoader();
+                throw Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
         }
-
-    } catch (error) {
+ } catch (error) {
         iziToast.error({
             title: 'Error',
             message: error.message,
         });
     } finally {
-         loader.classList.add('hidden');  
+         hideLoader();  
         
  } 
     formElem.reset();
    
 });
 
+async function handleUpdate() {
+  showLoader();
+  hideloadMoreButton();
+
+    try {
+    currentPage += 1;
+    const data = await searchImages(query, currentPage);
+    if (data.hits.length > 0) {
+      addImage(data.hits);
+        checkloadMoreButton();
+     const cardHeight = document
+    .querySelector('.js-images-container')
+    .getBoundingClientRect().height;
+      window.scrollBy({ top: cardHeight * 2, behavior: 'smooth' });   
+     
+        return
+        
+    } else {
+        throw new Error("We're sorry, there are no more posts to load");
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to fetch images. Please try again later.',
+    });
+  } finally {
+    hideLoader();
+  }
+}
 
 
+  
+function checkloadMoreButton() {
+    if (totalPages > currentPage) {
+        showloadMoreButton();
+    } else {
+        hideloadMoreButton();
+        iziToast.error({
+            title: 'Error',
+            message: "We're sorry, but you've reached the end of search results.",
+        });
+    }
+}
 
 function showLoader() {
   loader.style.display = 'block';
@@ -68,4 +113,15 @@ function showLoader() {
 function hideLoader() {
     loader.style.display = 'none';
 }
+
+
+
+function showloadMoreButton() {
+  loadMoreButton.classList.remove('hidden');
+}
+
+function hideloadMoreButton() {
+    loadMoreButton.classList.add('hidden');
+}
+
 
